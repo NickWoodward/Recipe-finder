5efe64aa1df6d55896054693d519e6a6d328c34a
Nick Woodward <nick.woodward@hotmail.co.uk>
2019-07-26 14:48:34 +0100

    updating project structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
 // Global app controller
 import "../style.css";
-import num from './test';
-
-const x = 233;
-
-console.log(`I imported ${num} from another module! Variable ${x}`);
+import '../assets/icons.svg';
