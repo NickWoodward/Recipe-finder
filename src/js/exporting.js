a30ae6d964b53d6ca95c5ab93e24e546c55222bc
Nick Woodward <nick.woodward@hotmail.co.uk>
2019-08-06 22:47:03 +0100

    structure change

--- src/js/exporting.js
+++
@@ -1,6 +0,0 @@
-import string from './models/Search';
-// import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView';
-
-console.log(string);
-console.log(searchView.add(1,2), searchView.multiply(2, 2), searchView.ID + 2);
