ab9657c67886b59982d612817039077628f97577
Nick Woodward <nick.woodward@hotmail.co.uk>
2019-09-07 12:52:46 +0100

    Formatting the shopping list

--- src/js/views/shoppingListView.js
+++ src/js/views/shoppingListView.js
@@ -1,13 +1,15 @@
 import { elements } from './base';
+import * as displayHelper from './displayHelper';
 
 export const renderItem = item => {
+    console.log(item);
     const markup = `
         <li class="shopping__item" data-itemid=${item.id}>
             <div class="shopping__count">
-                <input type="number" value="${item.qty}" step="${item.qtyPerServing}" class="shopping__count-value">
-                <p>${item.unit}</p>
+                <input type="number" dir="rtl" value="${displayHelper.formatNum(item.weightPerServing)}" step="1" class="shopping__count-value">
+                <p>g</p>
             </div>
-            <p class="shopping__description">${item.food}</p>
+            <p class="shopping__description">${displayHelper.capitalise(item.food)}</p>
             <button class="shopping__delete btn-tiny">
                 <svg>
                     <use href="#icons_icon-circle-with-cross"></use>
