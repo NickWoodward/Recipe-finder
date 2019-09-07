17a15aba44bc328238f77cafab046fa37fb586e4
Nick Woodward <nick.woodward@hotmail.co.uk>
2019-09-07 12:50:09 +0100

    Moving formatting functionality

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import { elements } from './base';
-import Fraction from 'fraction.js';
+import * as displayHelper from './displayHelper';
+
 
 export const clearRecipe = () => {
     elements.recipe.innerHTML = '';
@@ -100,11 +101,11 @@ const parseIngredient = ingredient => {
     result.food = result.food.toLowerCase();
 
     // Parse measurements to take plurals and language edge cases into account
-    result.unit = parseMeasurement(result.quantity, result.unit);
+    result.unit = displayHelper.parseMeasurement(result.quantity, result.unit);
 
     // Format numbers: .1 dp for weight, into {Fraction} for quantity
-    if (result.quantity) result.quantity = formatNum(result.quantity, 'qty');
-    if (result.weight) result.weight = formatNum(result.weight, 'weight');
+    if (result.quantity) result.quantity = displayHelper.formatNum(result.quantity, 'qty');
+    if (result.weight) result.weight = displayHelper.formatNum(result.weight, 'weight');
 
     /**
      * - 'qty-unit': Display the quantity of the food, plus the unit provided by the API
@@ -116,58 +117,13 @@ const parseIngredient = ingredient => {
         case 'qty-unit': return `${result.quantity} ${result.unit} of ${result.food}`;
         //NB: Wrong?
         case 'food-unit': return `${result.quantity} ${result.unit} (${result.weight}g)`; // deal with formatting probs here
-        case 'to-taste': return `${capitalise(result.food)} (To taste)`;
-        case 'food-weight': return `${capitalise(result.food)} (${result.weight}g)`;
+        case 'to-taste': return `${displayHelper.capitalise(result.food)} (To taste)`;
+        case 'food-weight': return `${displayHelper.capitalise(result.food)} (${result.weight}g)`;
         default: console.log('Unknown');
     }
 };
 
-const parseMeasurement = (quantity, measurement) => {
 
-    // Ignore <unit> as a measurement
-    if (measurement === '<unit>' || !quantity) return measurement;
-    // If the quantity is > 1 and doesn't already end with an 's', make the measurement plural
-    else if ((quantity > 1) && measurement.charAt(measurement.length - 1) !== 's') {
-        // Add unusual cases here
-        switch (measurement) {
-            case 'leaf': return 'leaves';
-            default: return `${measurement}s`;
-        }
-        // If the quantity is .5: Eg: 1/2 a cup
-    } else if (quantity === 0.5) {
-        return `a ${measurement}`;
-        // If the quantity < 1: Eg: 1/3 of a cup
-    } else if (quantity < 1) {
-        return `of a ${measurement}`;
-    } else {
-        return measurement;
-    }
-}
-
-const formatNum = (num, type) => {
-    // If type is a weight, use toFixed to .1
-    // If type is quantity, use the fraction.js package
-    if (type === 'weight') {
-        num = parseInt(num).toFixed(1);
-        // Remove the decimal if it's 0 
-        // 428.0 > 4280 > 0 > 0 === true
-        // 428.1 > 4281 > 1 > .1 === false
-        if (num * 10 % 10 / 10 === 0) {
-            return parseInt(num).toFixed(0);
-        } else {
-            return parseInt(num, 10);
-        }
-    } else if (type == 'qty') {
-        const fraction = new Fraction(num).simplify(0.001);
-        return fraction.toFraction(true);
-    }
-    return parseInt(num, 10);
-};
-
-const capitalise = word => {
-    if(word)
-        return word.charAt(0).toUpperCase() + word.slice(1);
-};
 
 
 
